409e293c05acc2f31d8743af494049ba3ee0a22d
Bennett <35482813+Bkielbon@users.noreply.github.com>
2018-06-13 21:39:41 -0700

    Update Assignment6.java

--- Assignment6/Assignment6.java
+++ Assignment6/Assignment6.java
@@ -7,46 +7,54 @@
 import java.awt.List;
 import java.util.*;
 
+
 public class Assignment6q {
+	//4. To which complexity class does the following algorithm belong? Consider N to be the length or size of the array or collection passed to the method. Explain your reasoning.
 	
-			//4. To which complexity class does the following algorithm belong? Consider N to be the length or size of the array or collection passed to the method. Explain your reasoning.
-	
-			   public static int[] mystery1(int[] list) {
-			       int[] result = new int[2 * list.length];
-			       for (int i = 0; i < list.length; i++) {
-			           result[2 * i] = list[i] / 2 + list[i] % 2;
-			           result[2 * i + 1] = list[i] / 2;
-			       }
-			 
-			       return result;
-			   }
-			   
-			   
-			//5. To which complexity class does the following algorithm belong?
-			   public static void mystery2(int[] list) {
-			       for (int i = 0; i < list.length / 2; i++) {
-			           int j = list.length - 1 - i;
-			           int temp = list[i];
-			           list[i] = list[j];
-			           list[j] = temp;
-			       } 
-			   }
-			   
-			//6. To which complexity class does the following algorithm belong?
-			   public static void mystery3(List<String> list) {
-			       for (int i = 0; i < list.size() - 1; i += 2) {
-			           String first = list.remove(i);
-			           list.add(i + 1, first);
-			       }
-			}
-			   
-			//7. To which complexity class does the following algorithm belong?
-			   public static void mystery4(List<String> list) {
-			       for (int i = 0; i < list.size() - 1; i += 2) {
-			           String first = list.get(i);
-			           list.set(i, list.get(i + 1));
-			           list.set(i + 1, first);
-			       }
-			 }
+	   public static int[] mystery1(int[] list) {
+	       int[] result = new int[2 * list.length];
+	       for (int i = 0; i < list.length; i++) {
+	           result[2 * i] = list[i] / 2 + list[i] % 2;
+	           result[2 * i + 1] = list[i] / 2;
+	       }
+	 
+	       return result;
+	   }
+	   
+	   //This algorithm is O(N) because complexity grows in direct proportion to N 
+	   
+	   
+	//5. To which complexity class does the following algorithm belong?
+	   public static void mystery2(int[] list) {
+	       for (int i = 0; i < list.length / 2; i++) {
+	           int j = list.length - 1 - i;
+	           int temp = list[i];
+	           list[i] = list[j];
+	           list[j] = temp;
+	       } 
+	   }
+	   
+	   //This algorithm is O(N) because complexity grows in direct proportion to N 
+
+	   
+	//6. To which complexity class does the following algorithm belong?
+	   public static void mystery3(List<String> list) {
+	       for (int i = 0; i < list.size() - 1; i += 2) {
+	           String first = list.remove(i);
+	           list.add(i + 1, first);
+	       }
+	}
+	   //This algorithm is O(N2) because when things are stored and removed from the middle of the list it has to shift elements before or after causing significant complexity
+	   
+	   
+	//7. To which complexity class does the following algorithm belong?
+	   public static void mystery4(List<String> list) {
+	       for (int i = 0; i < list.size() - 1; i += 2) {
+	           String first = list.get(i);
+	           list.set(i, list.get(i + 1));
+	           list.set(i + 1, first);
+	       }
+	 }
+	   ////This algorithm is O(N) becauase despite doing basically the same thing as mystery3 it swaps out items without removing them and having to shift the elements in the list 
 
 }
